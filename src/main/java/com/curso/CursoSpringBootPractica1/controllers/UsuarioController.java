ad405a746be21fabb249e5c0fdae7b1251896f2a
GrangerVMV <virginia.marchena@gmail.com>
2024-07-22 18:28:38 +0200

    Curso Spring Boot Practica 1: Función prueba añadida en Clase UsuarioController

--- src/main/java/com/curso/CursoSpringBootPractica1/controllers/UsuarioController.java
+++ src/main/java/com/curso/CursoSpringBootPractica1/controllers/UsuarioController.java
@@ -1,4 +1,12 @@
 package com.curso.CursoSpringBootPractica1.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UsuarioController {
+    @RequestMapping(value = "prueba")
+    public String prueba(){
+        return "prueba";
+    }
 }
