4b380b60536587b81247a6a17d3df8c9ac0c2e82
GrangerVMV <virginia.marchena@gmail.com>
2024-07-25 18:10:00 +0200

    Curso Spring Boot Practica 1: Package models y clase Usuario añadidas

---
+++ src/main/java/com/curso/CursoSpringBootPractica1/models/Usuario.java
@@ -0,0 +1,55 @@
+package com.curso.CursoSpringBootPractica1.models;
+
+public class Usuario {
+
+    // Atributos de la clase Usuario
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    private String password;
+
+    // Constructor de la clase Usuario
+
+    // Métodos de la clase Usuario incluyendo getters y setters
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getApellido(){
+        return apellido;
+    }
+
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getTelefono(){
+        return telefono;
+    }
+
+    public void setTelefono(String telefono){
+        this.telefono = telefono;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+}
